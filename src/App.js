d5f48e8052a9a4b6569e45a8d5047d5ec974c919
Gabriel <knuckles4x@gmail.com>
2020-12-19 19:47:26 -0300

    only 15 seconds of confetti

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import useWindowSize from "react-use/lib/useWindowSize";
 import Confetti from "react-confetti";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pieces, setPieces] = useState(200);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setPieces(0);
+    }, 15000);
+    return () => clearTimeout(timer);
+  }, []);
+
   const { width, height } = useWindowSize();
-  return <Confetti width={width} height={height} />;
+  return <Confetti width={width} height={height} numberOfPieces={pieces} />;
 }
 
 export default App;
